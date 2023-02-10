b86716c1a67bb42e07d76e640dffd98e5c8dd229
lordinvincible <125074067+lordinvincible@users.noreply.github.com>
2023-02-10 10:46:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('my parents new healthy dinner')
 streamlit.header('breakfirst menu')
 streamlit.text('Omega 3')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
